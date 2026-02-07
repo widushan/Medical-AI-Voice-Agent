f9c875b4c3b3e314c9e72a9780eb2819b8c5c019
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-02-08 00:50:34 +0530

    call commit
    Started to build call conversation ui

--- ai-medical-voice-agent/app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ ai-medical-voice-agent/app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -1,11 +1,23 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { doctorAgent } from '../../_components/DoctorAgentCard';
+import { Circle } from 'lucide-react';
+
+type SessionDetail = {
+  id: number,
+  notes: string,
+  sessionId: string,
+  report: JSON,
+  selectedDoctor: doctorAgent,
+  createdOn: string
+}
 
 function MedicalVoiceAgent() {
 
   const { sessionId } = useParams();
+  const [sessionDetail, setSessionDetail] = useState<SessionDetail>();
 
   useEffect(() => {
     sessionId && GetSessionDetails();
@@ -14,10 +26,16 @@ function MedicalVoiceAgent() {
   const GetSessionDetails = async () => {
     const result = await axios.get('/api/session-chat?sessionId=' + sessionId);
     console.log(result.data);
+    setSessionDetail(result.data);
   }
 
   return (
-    <div>{sessionId}</div>
+    <div>
+      <div>
+        <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle />Not Connected</h2>
+        <h2>00 : 00</h2>
+      </div>
+    </div>
   )
 }
 
