bec7c15471f049294f19ad51489824b6ccad5063
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-02-16 01:10:49 +0530

    dcard commit
    Managed Doctor Card Function

--- ai-medical-voice-agent/app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ ai-medical-voice-agent/app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -1,9 +1,13 @@
 "use client"
 
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { IconArrowRight } from '@tabler/icons-react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { Loader2Icon } from 'lucide-react'
+
 
 export type doctorAgent = {
   id: number,
@@ -18,12 +22,35 @@ type props = {
 }
 
 function DoctorAgentCard({doctorAgent}: props) {
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+
+  const onStartConsultation = async () => {
+    setLoading(true);
+    // Save All Info To Databse
+    const result = await axios.post('/api/session-chat', {
+      notes: 'New Query',
+      selectedDoctor: doctorAgent
+    });
+    console.log(result.data)
+    if (result.data?.sessionId) {
+      console.log(result.data.sessionId);
+      //Route new Conversation Screen
+      router.push('/dashboard/medical-agent/' + result.data.sessionId);
+
+    }
+    setLoading(false);
+  }
+
   return (
     <div className='border border-gray-200 rounded-lg p-4'>
       <Image src={doctorAgent.image} alt={doctorAgent.specialist} width={200} height={300} className='w-full h-[300px] object-cover rounded-xl'/>
       <h2 className='font-bold mt-1'>{doctorAgent.specialist}</h2>
       <p className='line-clamp-2 text-sm text-gray-500'>{doctorAgent.description}</p>
-      <Button className='w-full mt-2'>Start Consultation <IconArrowRight /></Button>
+      <Button className='w-full mt-2' onClick={onStartConsultation}>Start Consultation {loading?<Loader2Icon className='animate-spin'/>: <IconArrowRight />}</Button>
     </div>
   )
 }
