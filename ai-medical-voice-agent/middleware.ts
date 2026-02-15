592964479ec34aa66a96dff9ea46333d6fae59a4
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-02-16 01:12:44 +0530

    route commit
    Updated routes

--- ai-medical-voice-agent/middleware.ts
+++ ai-medical-voice-agent/middleware.ts
@@ -6,6 +6,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
